6223e9020c2a2d2347cb5dfbeaf43f8bbb644078
Teo Pinillo <teopinillo@gmail.com>
2021-01-08 06:39:55 -0500

    post model

--- message-board/message_board_project/posts/admin.py
+++ message-board/message_board_project/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fields = ('comment',)
+
+admin.site.register (Post, PostAdmin)
+
--- message-board/message_board_project/posts/models.py
+++ message-board/message_board_project/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post (models.Model):
+    date_created = models.DateTimeField (auto_now_add=True, null=True)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.comment[:50]
+
